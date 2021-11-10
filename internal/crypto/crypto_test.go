027e4e74cd485958e9b97ebc7b3db04c583d36ec
Glen Murdock <gemurdock@gmail.com>
2021-11-10 16:26:27 -0500

    Added more tests

--- internal/crypto/crypto_test.go
+++ internal/crypto/crypto_test.go
@@ -5,20 +5,40 @@ import (
 )
 
 func TestEncode(t *testing.T) {
-	var text = "Hello World!"
-	var password = "password"
 	var goal = "v83m5u2W0Nrp1N6b"
-	var result, err = encode(text, password)
+	var result, err = encode("Hello World!", "password")
+	if result != goal || err != nil {
+		t.Errorf("Test Encode Failed: %s != %s", result, goal)
+	}
+
+	goal = "q6q6m2mefZGOSJtvu72zlpBqhoiR"
+	result, err = encode("CAPS TEXT TESTING NOW", "abcABC123!@#")
+	if result != goal || err != nil {
+		t.Errorf("Test Encode Failed: %s != %s", result, goal)
+	}
+
+	goal = "aseFvphopnWFvYrVo13MhrDIm7c="
+	result, err = encode("!CW^),2:<9\\u4!XKgDmW", "B}'Yh5m4")
 	if result != goal || err != nil {
 		t.Errorf("Test Encode Failed: %s != %s", result, goal)
 	}
 }
 
 func TestDecode(t *testing.T) {
-	var text = "v83m5u2W0Nrp1N6b"
-	var password = "password"
 	var goal = "Hello World!"
-	var result, err = decode(text, password)
+	var result, err = decode("v83m5u2W0Nrp1N6b", "password")
+	if result != goal || err != nil {
+		t.Errorf("Test Decode Failed: %s != %s", result, goal)
+	}
+
+	goal = "CAPS TEXT TESTING NOW"
+	result, err = decode("q6q6m2mefZGOSJtvu72zlpBqhoiR", "abcABC123!@#")
+	if result != goal || err != nil {
+		t.Errorf("Test Decode Failed: %s != %s", result, goal)
+	}
+
+	goal = "!CW^),2:<9\\u4!XKgDmW"
+	result, err = decode("aseFvphopnWFvYrVo13MhrDIm7c=", "B}'Yh5m4")
 	if result != goal || err != nil {
 		t.Errorf("Test Decode Failed: %s != %s", result, goal)
 	}
